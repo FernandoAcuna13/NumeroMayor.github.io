e31249c4a83499f7d1a06f36f1d348af39298de9
FernandoAcuña <144246351+FernandoAcuna13@users.noreply.github.com>
2023-09-09 01:07:25 -0400

    initial commit

---
+++ num.js
@@ -0,0 +1,14 @@
+var n1,n2,n3,t,mayor;
+	n1 = parseInt(prompt("Número 1"));
+	n2 = parseInt(prompt("Número 2"));
+	n3 = parseInt(prompt("Número 3"));
+
+if (n1>n2){
+    if (n1 > n2 || n1>n3) {
+		console.log("El numero mayor es: "+n1);
+	}else if(n2 > n1 || n2>n3){
+		console.log("El numero mayor es: "+n2);
+	}else if(n3 > n1 || n3>n2){
+        console.log("El numero mayor es: "+n3);
+    }
+}
